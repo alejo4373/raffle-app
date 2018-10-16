77b7b03c926158efa652261cad773562779375f8
alejo4373 <alejo4373@gmail.com>
2018-10-16 19:14:28 -0400

    database schema started definition

---
+++ db/users.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS colombiafest_users;
+CREATE DATABASE colombiafest_users;
+
+\c colombiafest_users;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    lastname VARCHAR UNIQUE NOT NULL,
+    email VARCHAR NOT NULL,
+    phone VARCHAR NOT NULL,
+    registered_at VARCHAR
+);
+
+INSERT INTO users (name, lastname, email, phone, registered_at)
+VALUES('Alejandro', 'Franco', 'alejandrofranco@ac.c4q.nyc', '917-5736025','1539731307708'),
+      ('Kelvin', 'Rodriguez', 'kelvinrodriguez@ac.c4q.nyc', '917-5736025','1539731307708'),
+      ('Joyce', 'Ajagbe', 'joyceajagbe@ac.c4q.nyc', '917-5736025','1539731307708');
