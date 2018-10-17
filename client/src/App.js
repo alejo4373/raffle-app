2e6e759d6bf8bf357ac4afd200037f72c4fd1487
alejo4373 <alejo4373@gmail.com>
2018-10-16 21:34:28 -0400

    form scaffolding

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Container, Form } from 'semantic-ui-react';
 
 class App extends Component {
+  state = {
+
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
+        <Container className='header'>
+          <img src='http://www.digital.nyc/sites/all/themes/custom/ecohub_foundation_dnyc/images/nyc_bg.jpg' alt='nyc skyline' />
+        </Container>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Field>
+            <label>First Name:</label>
+            <input placeholder='First Name' />
+          </Form.Field>
+          <Form.Field>
+            <label>Last Name:</label>
+            <input placeholder='Last Name' />
+          </Form.Field>
+          <Form.Field>
+            <label>Email:</label>
+            <input type='email' placeholder='First Name' />
+          </Form.Field>
+          <Form.Field>
+            <label>First Name:</label>
+            <input placeholder='First Name' />
+          </Form.Field>
+        </Form>
       </div>
     );
   }
