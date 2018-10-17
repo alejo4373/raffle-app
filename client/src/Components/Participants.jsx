f3b65771bf218b46fbc75e1f5adaa09f23d211eb
alejo4373 <alejo4373@gmail.com>
2018-10-17 14:27:02 -0400

    participants search

--- client/src/Components/Participants.jsx
+++ client/src/Components/Participants.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { List, Image, Header } from 'semantic-ui-react';
+import { List, Image, Header, Input } from 'semantic-ui-react';
 
 class Participants extends Component {
   state = {
+    allParticipants: [],
     participants: []
   }
 
@@ -11,12 +12,29 @@ class Participants extends Component {
     axios.get('/users')
       .then(({ data }) => {
         this.setState({
+          allParticipants: data,
           participants: data
         })
       })
       .catch(err => {
         console.log('Error fetching users', err);
       })
+  }
+
+  filterParticipants = (pattern) => {
+    this.setState(prevState => {
+      const filteredParticipants = prevState.allParticipants.filter(p => {
+        const fullName = p.name + ' ' + p.lastname;
+        return fullName.toLowerCase().includes(pattern.toLowerCase())
+      })
+
+      return { participants: filteredParticipants }
+    })
+  }
+
+  handleInput = (e, { value }) => {
+    console.log(value)
+    this.filterParticipants(value);
   }
 
   componentDidMount = () => {
@@ -28,9 +46,12 @@ class Participants extends Component {
     return (
       <div>
         <Header as='h2'>Participants: </Header>
+        <div>
+          <Input fluid icon='search' placeholder='Search..' onChange={this.handleInput} />
+        </div>
         <List divided>{
-          participants.map(p => (
-            <List.Item>
+          participants.map((p, i) => (
+            <List.Item key={p.name + i}>
               <Image circular className='user-avatar' src='/avatar.svg' />
               <List.Content>
                 <List.Header as='h4'>{`${p.name} ${p.lastname}`}</List.Header>
