802bd19c51c6f5ee4f84974b6acf3179ca6dd2f6
alejo4373 <alejo4373@gmail.com>
2018-10-20 06:21:24 -0400

    top banner change + bottom banner add

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Segment } from 'semantic-ui-react';
+import { Segment, Image } from 'semantic-ui-react';
 import './App.css';
 
 // Child Components
@@ -20,6 +20,8 @@ class App extends Component {
             <Route path='/raffle' component={Raffle} />
             <Route path='/' component={FormComponent} />
           </Switch>
+          <br />
+          <Image fluid src='/footer.jpg' alt='tamales' />
         </Segment>
       </div>
     );
