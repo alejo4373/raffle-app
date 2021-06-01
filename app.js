bbcb57194828a6a2069685afc13a43bb15e619f9
alejo4373 <alejandrofranco@pursuit.org>
2021-06-01 18:53:27 -0400

    Add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 if (process.env.NODE_ENV === "development") {
   require('dotenv').config()
@@ -10,6 +11,7 @@ if (process.env.NODE_ENV === "development") {
 const app = express();
 const rafflesRouter = require('./routes/raffles')
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
